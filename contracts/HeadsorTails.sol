4b56c6d136ff7dc88c405637cef1230f0e9ef6f4
Anik Ghosh <itsmenick30@gmail.com>
2022-03-10 10:48:53 +0530

    updated

--- contracts/HeadsorTails.sol
+++ contracts/HeadsorTails.sol
@@ -1,35 +1,26 @@
-// Version of Solidity compiler this program was written for
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Heads or tails game contract
-contract HeadsOrTails {
-  address payable owner;
-  string public name;
+contract HeadsOrTails is Ownable{
+    struct Game {
+        address addr;
+        uint amountBet;
+        uint8 guess;
+        bool winner;
+        uint ethInJackpot;
+    }
 
-  struct Game {
-    address addr;
-    uint amountBet;
-    uint8 guess;
-    bool winner;
-    uint ethInJackpot;
-  }
+  Game[] public lastPlayedGames;
 
-  Game[] lastPlayedGames;
+    ///@notice This returns the input user enters and the outcome.
+    event GameResult(uint8 indexed side, bool indexed isWon);
 
-  //Log game result (heads 0 or tails 1) in order to display it on frontend
-  event GameResult(uint8 indexed side);
-
-  // Contract constructor run only on contract creation. Set owner.
-  constructor() {
-    owner = msg.sender;
-  }
-
-  //Play the game!
-  function lottery(uint8 guess) public payable returns(bool){
-    require(guess == 0 || guess == 1, "Variable 'guess' should be either 0 ('heads') or 1 ('tails')");
+  ///@notice Play the game!
+  function lottery(uint8 guess) public payable{
+    require(guess < 2, "Variable 'guess' should be either 0 ('heads') or 1 ('tails')");
     require(msg.value > 0, "Bet more than 0");
-    require(msg.sender == tx.origin,"Contract call not allowed");
+      require(isContract(_msgSender()) == true, "Contract call not allowed");
     require(msg.value <= address(this).balance - msg.value, "You cannot bet more than what is available in the jackpot");
     //address(this).balance is increased by msg.value even before code is executed. Thus "address(this).balance-msg.value"
     //Create a random number. Use the mining difficulty & the player's address, hash it, convert this hex to int, divide by modulo 2 which results in either 0 or 1 and return as uint8
@@ -37,13 +28,12 @@ contract HeadsOrTails {
     bool won;
     if (guess == result) {
       //Won!
-      payable(msg.sender).transfer(msg.value * 2);
+      payable(_msgSender()).transfer(msg.value * 2);
       won = true;
     }
 
     lastPlayedGames.push(Game(msg.sender, msg.value, guess, won, address(this).balance));
-    emit GameResult(result);
-    return won; //Return value can only be used by other functions, but not within web3.js (as of 2019)
+    emit GameResult(result, won);
   }
 
   //Get amount of games played so far
@@ -51,28 +41,23 @@ contract HeadsOrTails {
     return lastPlayedGames.length;
   }
 
-  //Get stats about a certain played game, e.g. address of player, amount bet, won or lost, and ETH in the jackpot at this point in time
-  function getGameEntry(uint index) public view returns(address addr, uint amountBet, uint8 guess, bool winner, uint ethInJackpot) {
-    return (
-      lastPlayedGames[index].addr,
-      lastPlayedGames[index].amountBet,
-      lastPlayedGames[index].guess,
-      lastPlayedGames[index].winner,
-      lastPlayedGames[index].ethInJackpot
-    );
-  }
+    function isContract(address addr) internal view returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
 
   // Contract destructor (Creator of contract can also destroy it and receives remaining ether of contract address).
   //Advantage compared to "withdraw": SELFDESTRUCT opcode uses negative gas because the operation frees up space on
   //the blockchain by clearing all of the contract's data
   function destroy() external onlyOwner {
-    selfdestruct(owner);
+    selfdestruct(payable(owner()));
   }
 
   //Withdraw money from contract
-  function withdraw(uint amount) external onlyOwner {
+  function withdraw(uint amount) external {
     require(amount < address(this).balance, "You cannot withdraw more than what is available in the contract");
-    owner.transfer(amount);
+    payable(owner()).transfer(amount);
   }
 
   function vrf() private view returns (bytes32 result) {
@@ -87,7 +72,6 @@ contract HeadsOrTails {
         }
         return result;
     }
-
 
   // Accept any incoming amount
   receive() external payable {}
